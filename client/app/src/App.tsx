4028c7b021f7d5e815cdda6086fcd495c05d63a3
icy1xtee <easton2904@mail.ru>
2024-02-23 01:49:00 +0300

    modal and incorrect page

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,48 +1,31 @@
 import React, { useEffect } from 'react';
-
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
 import Main from './features/Main/Main';
-
 import QuizMainPage from './features/Quiz/components/QuizMainPage';
 import Registration from './features/Auth/Components/Registration';
 import Login from './features/Auth/Components/Login';
 import * as api from "./features/Auth/api"
-import { useDispatch } from 'react-redux';
-
-
-
-
+import IncorrectPage from './features/Incorrect/IncorrectPage';
 
 function App(): JSX.Element {
+  const dispatch = useDispatch()
 
-const dispatch = useDispatch()
-
-useEffect(() => {
-
-  
-  api.userfetch().then(data => dispatch({type: 'auth/user', payload:data})).catch(console.log)
-  
-},[])
-
-
-
+  useEffect(() => {
+    api.userfetch().then(data => dispatch({type: 'auth/user', payload:data})).catch(console.log)
+  },[])
 
   return (
-
-<Routes>
-<Route path='/' element={<Main />}>
-<Route index element={<QuizMainPage  />}/>
- <Route path="/registration" element={<Registration />} />
- <Route path="/login" element={<Login />} />
-
-</Route>
-
-</Routes>
-
-)
-
+    <Routes>
+    <Route path='/' element={<Main />}>
+    <Route index element={<QuizMainPage  />}/>
+    <Route path="/registration" element={<Registration />} />
+    <Route path="/login" element={<Login />} />
+    <Route path='*' element={<IncorrectPage />}/>
+    </Route>
+    </Routes>
+  )
 }
 
 export default App;
