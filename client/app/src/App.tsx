3ac8fee6321503629f59e66e3f3274eb948eb577
Nikita <nik.aksenovv@gmail.com>
2024-02-22 14:46:16 +0300

    router

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Main from './features/Main/Main';
 
 function App(): JSX.Element {
+  const dispatch = useDispatch()  
   
   return (
+<Routes>
+<Route path='/' element={<Main/>}>
 
-  )
+
+</Route>
+
+</Routes>
+
+)
 }
 
 export default App;
