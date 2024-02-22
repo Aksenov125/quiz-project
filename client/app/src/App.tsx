729c11b52a823f6965429c3fca80c06112275b9a
Nikita <nik.aksenovv@gmail.com>
2024-02-22 15:44:50 +0300

    components

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 import './App.css';
-import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Main from './features/Main/Main';
 
+
+
 function App(): JSX.Element {
-  const dispatch = useDispatch()  
+
   
   return (
 <Routes>
-<Route path='/' element={<Main/>}>
-
-
-</Route>
+<Route path='/' element={<Main/>} />
 
 </Routes>
 
---
+++ client/app/src/features/Quiz/components/QuizMainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuizMainPage():JSX.Element {
+  return (
+    <div>QuizMainPage</div>
+  )
+}
+
+export default QuizMainPage
