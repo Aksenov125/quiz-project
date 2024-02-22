42467b92a79871d2ab9e8a785df77ee19123d82e
icy1xtee <easton2904@mail.ru>
2024-02-22 19:49:56 +0300

    fix bug

--- client/app/src/features/Quiz/components/QuizMainPage.tsx
+++ client/app/src/features/Quiz/components/QuizMainPage.tsx
@@ -8,23 +8,22 @@ import QuestionCard from '../../Questions/components/QuestionCard.tsx';
 function QuizMainPage():JSX.Element {
 
   const dispatch = useDispatch()
-
   const themes = useSelector((store:RootState) => store.initialThemeState.themes)
-
- 
-  
   
   useEffect(() => {
+    console.log('lol',13124144141414)
     api.initFetchThemeAndQuestion().then(data => dispatch({type:'Theme/Init', payload:data})).catch(console.log)
 
   },[])
-  console.log(themes.themes[0].Questions, '1111');
+  
+
+  
 
 
   return (
     <div>
 {
-  themes.themes.map((theme) =><> <p>{theme.name}</p> {theme.Questions.map((question)=> <QuestionCard question={question}/>)}</>)
+  themes.map((theme) =><> <p key={theme.id}>{theme.name}</p> {theme.Questions.map((question)=> <QuestionCard key={question.id} question={question}/>)}</>)
 }
 
     </div>
