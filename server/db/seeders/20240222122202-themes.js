c12beb1a35dc37834ee0b0eebed15d0db4d72d4c
alyonaosipova <alyonaosipova@gmail.com>
2024-02-23 15:01:33 +0300

    table_score_css_and_theme5_seeds

--- server/db/seeders/20240222122202-themes.js
+++ server/db/seeders/20240222122202-themes.js
@@ -1,36 +1,40 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.bulkInsert('Themes', [
-      {
-        name: 'Правда или Ложь',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Theme2',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Theme3',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Theme4',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Theme5',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+    await queryInterface.bulkInsert(
+      "Themes",
+      [
+        {
+          name: "Правда или Ложь",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Theme2",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Theme3",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Theme4",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Принадлежности",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface) {
-    await queryInterface.bulkDelete('Themes', null, {});
+    await queryInterface.bulkDelete("Themes", null, {});
   },
 };
