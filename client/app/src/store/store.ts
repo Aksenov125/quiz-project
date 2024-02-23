c6c0d0433447342b6b322e60f8a74416556f1e65
icy1xtee <easton2904@mail.ru>
2024-02-23 15:09:07 +0300

    seeders done 1,3,4

--- client/app/src/store/store.ts
+++ client/app/src/store/store.ts
@@ -1,8 +1,9 @@
 import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import  rootReducer  from './rootReducer';
 
 // контейнер состояний — store
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
 
 // тип для контейнера состояний — store,
 // формируется с помощью утилиты ReturnType, которая получает тип возвращаемого значения
